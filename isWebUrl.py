56a7f7d39b17cdf03d972e71b011f39aab8a4ca4
creadits7 <31751689+creadits7@users.noreply.github.com>
2017-09-07 21:41:02 -0500

    Update isWebUrl.py

--- isWebUrl.py
+++ isWebUrl.py
@@ -1,24 +1,29 @@
 import re, pyperclip
 
+#Finds URLs beginning with www., http:// or https://
 webUrlRegex = re.compile(r'(http://|https://|www.)(www\.)?([a-zA-Z0-9-_.]+)(\.[a-zA-Z0-9]{2,4})(\S+)')
 
+#pastes from clipboard
 text = str(pyperclip.paste())
 
 
 mo = webUrlRegex.findall(text)
 
-headers = []
+protocols = []
 domains = []
-endings = []
-directories = []
+tlds = []
+paths = []
 urls = []
+
+#Splits the URL into 4 parts.
+#http://www. | example | .org | /examples/examplepictures.html
 
 for groups in mo:
     urls.append("".join(groups))
-    headers.append(groups[0])
+    protocols.append(groups[0])
     domains.append(groups[2])
-    endings.append(groups[3])
-    directories.append(groups[4])
+    tlds.append(groups[3])
+    paths.append(groups[4])
 
 
 print("URLs found:")
@@ -26,7 +31,7 @@ for i in urls:
     print(i)
 
 print("\n")
-print("Headers:")
+print("Protocols:")
 for i in headers:
     print(i)
 
@@ -38,12 +43,12 @@ for i in domains:
 
 print("\n")
 print("Endings:")
-for i in endings:
+for i in tlds:
     print(i)
 
 print("\n")
 print("Directories:")
-for i in directories:
+for i in paths:
     print(i)
 
 
